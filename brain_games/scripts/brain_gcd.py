3fcb4b9ef24d2e8de9509f774df399b73e72d103
Artemka1989 <kurnev.ar@yandex.ru>
2022-12-04 21:15:23 +0300

    improve brain-gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,41 @@
+#! usr/bin/env/ python3
+
+import prompt
+import random
+print("Welcome to the brain-games!")
+
+
+def main():
+    i = 0
+    name = prompt.string("May I have your name? ")
+    print('Hello, ' + name + '!')
+    print("Find the greatest common division of given numbers")
+    for i in range(1, 4):
+        num = random.randint(1, 100)
+        num_1 = random.randint(1, 100)
+        print(f"Question: {num} {num_1}")
+        answer = prompt.string("Your answer: ")
+        q = gcd_loop(num, num_1)
+        if str(q) == str(answer):
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{q}'")
+            print(f"Let's try again, {name}!")
+            break
+    if i == 3:
+        print(f"Congratulations, {name}!")
+
+
+def gcd_loop(num, num_1):
+    if num > num_1:
+        temp = num_1
+    else:
+        temp = num
+    for i in range(1, temp + 1):
+        if ((num % i == 0) and (num_1 % i == 0)):
+            gcd = i
+    return gcd
+
+
+if __name__ == "__main__":
+    main()
