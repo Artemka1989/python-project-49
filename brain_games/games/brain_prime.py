81bb20ced31450702348b5c7d6917982b0ca5fa2
Artemka1989 <kurnev.ar@yandex.ru>
2023-01-09 22:22:32 +0300

    brain_games/games/brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -7,12 +7,19 @@ MAX_NUM = 100  # Максимальное значение числа
 def play():
     num = random.randint(0, MAX_NUM)
     question = f'{num}'
+    if is_even(num):
+        calc = 'yes'
+    else:
+        calc = 'no'
+    return question, calc
+
+
+def is_even(num):
     count = 0
     for i in range(1, num + 1):
         if num % i == 0:
             count += 1
     if count <= 2:
-        calc = 'yes'
+        return True
     else:
-        calc = 'no'
-    return question, calc
+        return False
